5486b880cf3b7692bf5a3e5f06810c44a01beca2
Michael-B1764 <mikebailar@gmail.com>
2020-04-13 12:06:55 -0700

    all done with mvp. Will run some more queries for stretch and practice.

--- api/server.js
+++ api/server.js
@@ -6,4 +6,75 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/accounts', (req, res) => {
+    db('*').from('accounts')   
+    .then(accounts => {
+        res.status(200).json({data: accounts})
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({error: error.message})
+    })
+})
+
+server.get('/api/accounts/:id', (req, res) => {
+    const id = req.params.id
+    db('*').from('accounts')
+    .where({ id })  
+    .then(accounts => {
+        res.status(200).json({data: accounts})
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({error: error.message})
+    })
+})
+
+server.post('/api/accounts', (req, res) => {
+    const accountData = req.body
+    db('accounts').insert(accountData, 'id')
+    .then(ids => {
+        const id = ids[0]
+        db('accounts')
+        .where({ id })
+        .first()
+        .then(account => {
+            res.status(200).json({data: account})
+        })
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({error: error.message})
+    })
+})
+
+server.put('/api/accounts/:id', (req, res) => {
+    const changes = req.body
+    const { id } = req.params
+    db('accounts')
+    .where({ id })
+    .update(changes)
+    .then(count => {
+        if (count > 0) {
+            res.status(200).json({message: "update successful"})
+        } else {
+            res.status(404).json({message: "no accounts found with that id"})
+        }
+    })
+})
+
+server.delete('/api/accounts/:id', (req, res) => {
+    const { id } = req.params
+    db('accounts')
+    .where({ id })
+    .del()
+    .then(count => {
+        if (count > 0) {
+            res.status(200).json({message: "delete successful"})
+        } else {
+            res.status(404).json({message: "not posts found with that id"})
+        }
+    })
+})
+
 module.exports = server;
